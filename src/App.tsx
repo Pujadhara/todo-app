8f04aa4889cb71b51bb4d89a12bf2da7e570cc99
Pujadhara <dharapuja54@gmail.com>
2024-06-29 17:22:53 +0530

    scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import "./App.scss";
 import React from 'react';
 import CompletedTask from './components/CompletedTask';
 import TaskList from './components/TaskList';
@@ -12,7 +13,7 @@ const App: React.FC = () => {
         <div className="new_task_list task_list">
           <h2>Task List</h2>
           <div><TaskList/></div>
-        </div>
+        </div>  
         <div className="completed_task_list task_list">
           <h2>Completed Task</h2>
           <div><CompletedTask/></div>
